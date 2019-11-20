41d3157a1e9592fa614cd2a1823f6b9d2bf6f0c2
Waseem D. Haji <46052715+DeNyMus1NeBiLon@users.noreply.github.com>
2019-11-20 15:54:55 +0100

    Update mypage.py

--- mypage.py
+++ mypage.py
@@ -1,13 +1,17 @@
-import webbrowser
+import webbrowser  # Starts to import a Webbrowser
+
+# Creating class Movie for the movie trailer
 
 
 class Movie():
+    # initializing constructor with 4 arguments
     def __init__(self, movie_title, m_story_l, poster_image, trailer_youtube):
 
         self.title = movie_title
         self.storyline = m_story_l
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+# displaying trailer using webbrower.open method
 
     def show_trailer(self):
         print(self.trailer)
