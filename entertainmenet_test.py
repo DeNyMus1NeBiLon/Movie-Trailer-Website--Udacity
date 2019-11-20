6086880da702737aa720233bd1af1b408835f408
Waseem D. Haji <46052715+DeNyMus1NeBiLon@users.noreply.github.com>
2019-11-20 16:22:05 +0100

    Update entertainmenet_test.py

--- entertainmenet_test.py
+++ entertainmenet_test.py
@@ -1,10 +1,17 @@
-import fresh_tomatoes     # importing fresh_tomatoes
+# Importing the fresh_tomatoes module which helps
+# to generate & render the html page using python code
+import fresh_tomatoes
+
+# Importing the mypage module where the
+# constructer is initialized for movies as parameters
 import mypage
 
+# Creates 15 different movie trailer instance of Theatre class with
+# 4 parameters(movie_title, movie_storyline, poster_image, trailer_youtube)
 toy_story = mypage.Movie(
     "Toy Story",
     "A boy who have toys",
-    "https://vignette.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",
+    "https://vignette.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",  # noqa
     "https://www.youtube.com/watch?v=xmzbwheRutk")
 
 avatar = mypage.Movie(
@@ -28,20 +35,20 @@ rotatouille = mypage.Movie(
 midnight_in_paris = mypage.Movie(
     "Midnight in Paris",
     "Going back in time to meet Authors",
-    "https://vignette.wikia.nocookie.net/midnightinparis/images/a/a9/Midnight-in-paris-header.jpg/revision/latest?cb=20130424031150",
+    "https://vignette.wikia.nocookie.net/midnightinparis/images/a/a9/Midnight-in-paris-header.jpg/revision/latest?cb=20130424031150",  # noqa
     "https://www.youtube.com/watch?v=FAfR8omt-CY")
 
 hunger_games = mypage.Movie(
     "Hunger Games",
     "A really reality real show ",
-    "https://vignette.wikia.nocookie.net/dietributevonpanem/images/a/a5/250px-O-FINAL-HUNGER-GAMES-POSTER-570.jpg/revision/latest?cb=20120401064801&path-prefix=de",
+    "https://vignette.wikia.nocookie.net/dietributevonpanem/images/a/a5/250px-O-FINAL-HUNGER-GAMES-POSTER-570.jpg/revision/latest?cb=20120401064801&path-prefix=de",  # noqa
     "https://www.youtube.com/watch?v=4S9a5V9ODuY")
 
 gangs_of_ny = mypage.Movie(
     "Gangs of New York",
     "Amsterdam returns to Five Points ,"
     "to get revenge on his father's killer",
-    "https://upload.wikimedia.org/wikipedia/en/a/ae/Gangs_of_New_York_Poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/a/ae/Gangs_of_New_York_Poster.jpg",  # noqa
     "https://www.youtube.com/watch?v=qHVUPri5tjA")
 
 pulp_fiction = mypage.Movie(
@@ -49,7 +56,7 @@ pulp_fiction = mypage.Movie(
     "Storylines of mobsters,  "
     "fringe players, small-time criminals, "
     "and a mysterious briefcase",
-    "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",  # noqa
     "https://www.youtube.com/watch?v=tGpTpVyI_OQ")
 
 divergent = mypage.Movie(
@@ -78,7 +85,7 @@ allegiant = mypage.Movie(
 ascendant = mypage.Movie(
     "The Divergent Series: Ascendant",
     "After Allegiant is Ascendant which is to arrive in 2017",
-    "http://www.thedivergentseries.movie/ascendant/assets/img/poster/poster.jpg",
+    "http://www.thedivergentseries.movie/ascendant/assets/img/poster/poster.jpg",  # noqa
     "https://youtu.be/6DWqWGwIATQ")
 
 star_trek = mypage.Movie(
@@ -96,13 +103,13 @@ star_trek_into_darkness = mypage.Movie(
     "on the Federation, Captain Kirk (Chris Pine),  "
     "Spock (Zachary Quinto), and the crew of the U.S.S. Enterprise "
     "set out on their most explosive manhunt of all time.",
-    "https://upload.wikimedia.org/wikipedia/en/5/50/StarTrekIntoDarkness_FinalUSPoster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/5/50/StarTrekIntoDarkness_FinalUSPoster.jpg",  # noqa
     "https://youtu.be/sJNyGFqgyag")
 
 interstellar = mypage.Movie(
     "Interstellar",
     "A team of explorers travel through a wormhole in space in an",
-    "https://vignette.wikia.nocookie.net/interstellarfilm/images/6/6f/Interstellar_website.jpg/revision/latest?cb=20140923050808",
+    "https://vignette.wikia.nocookie.net/interstellarfilm/images/6/6f/Interstellar_website.jpg/revision/latest?cb=20140923050808",  # noqa
     "https://www.youtube.com/watch?v=zSWdZVtXT7E")
 
 #  Code Array, Array python LIST
@@ -122,5 +129,5 @@ movies = [
     star_trek,
     star_trek_into_darkness,
     interstellar]
-#  Display the moie page!
+#  Display the movie page!
 fresh_tomatoes.open_movies_page(movies)
